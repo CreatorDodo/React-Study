4df6b837da2f2e668b0d07a3424b1864afe1ca27
김도영 <difbfl4750@gmail.com>
2024-05-21 19:24:53 +0900

    reactStudy

--- src/index.js
+++ src/index.js
@@ -1,15 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+// import App from './App';
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// import Library from './chapter_03/Library';
+// import Clock from './chapter_04/Clock';
+// import CommentList from './chapter_05/CommentList';
+// import NotificationList from './chapter_06/NotificationList';
+// import Accommodate from './chapter_07/Accommodate';
+// import ConfirmButton from './chapter_08/ConfirmButton';
+// import LandingPage from './chapter_09/LandingPage';
+// import AttendanceBook from './chapter_10/AttendanceBook';
+// import SignUp from './chapter_11/SignUp';
+// import Calculator from './chapter_12/Calculator';
+// import ProfileCard from './chapter_13/ProfileCard';
+// import DarkOrLight from "./chapter_14/DarkOrLight";
+import Blocks from "./chapter_15/Blocks";
+
+const root = createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Blocks />
+  </React.StrictMode>,
+  document.getElementById("root")
 );
+
+// setInterval(() => {
+//   root.render(
+//     <React.StrictMode>
+//       <Clock />
+//     </React.StrictMode>,
+//     document.getElementById('root')
+//   );
+// }, 1000);
+
+// root.render(
+//   <React.StrictMode>
+//     <Library />
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
